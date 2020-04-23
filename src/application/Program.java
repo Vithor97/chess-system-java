6ce9400bfda5feab760438b35d88a271d79686e4
Vitor Miranda <vitorsantos27@hotmail.com>
2020-04-23 20:51:35 -0300

    teste

--- src/application/Program.java
+++ src/application/Program.java
@@ -13,7 +13,7 @@ import chess.ChessPosition;
 public class Program {
 
 	public static void main(String[] args) {
-//teste
+//release
 		Scanner sc = new Scanner(System.in);
 		ChessMatch chessMatch = new ChessMatch();
 		List<ChessPiece> captured = new ArrayList<>();
